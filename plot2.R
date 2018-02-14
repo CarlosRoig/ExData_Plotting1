cb19f95c38767939483676856f44f8c69ab6feb1
Carlos Roig <carlos.roig@godesys.de>
2018-02-14 14:36:25 +0100

    added plot2 R and png

---
+++ plot2.R
@@ -0,0 +1,8 @@
+data <- fread("./household_power_consumption.txt")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+febData <- subset(data, Date == as.Date("01/02/2007", "%d/%m/%Y") | Date == as.Date("02/02/2007", "%d/%m/%Y"))
+febData$Global_active_power <- as.numeric(febData$Global_active_power)
+plot(febData$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xaxt="n", xlab = "")
+axis(1, at= c(1, length(febData$Global_active_power)/2, length(febData$Global_active_power)), labels = c("Thu", "Fri", "Sat"))
+dev.copy(png, "plot2.png")
+dev.off()
